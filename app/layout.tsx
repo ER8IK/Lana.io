faeb17d73f251527c780f93790b3fa6e786ab44f
ER8IK <locha.eth@gmail.com>
2025-11-13 22:31:59 +0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon.ico", type: "image/x-icon" }, // стандартный favicon
+      { url: "/CircuitLabs.ico", type: "image/x-icon" }, // стандартный favicon
       { url: "/android-chrome-192x192.png", type: "image/png", sizes: "192x192" },
       { url: "/android-chrome-512x512.png", type: "image/png", sizes: "512x512" },
     ],
