a85e5333973a510d27e4d57ee2043b2a0aa99bbd
ER8IK <locha.eth@gmail.com>
2025-11-13 22:23:19 +0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon.ico", type: "image/x-icon" }, // стандартный favicon
       { url: "/android-chrome-192x192.png", type: "image/png", sizes: "192x192" },
-      { url: "/android-chrome-512x512.png", type: "image/png", sizes: "512x512" },
+      { url: "/android-chrome-512x512.png?v=2", type: "image/png", sizes: "512x512" }
     ],
     apple: "/apple-touch-icon.png", // иконка для iOS
   },
